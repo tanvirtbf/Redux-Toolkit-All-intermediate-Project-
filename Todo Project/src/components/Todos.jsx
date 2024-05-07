c79190943c7bc0357fa8f7f6c0a3810809bab898
tanvirtbf <tanver018765@gmail.com>
2024-05-07 11:22:57 +0600

    remove functionailty apply on ui

--- Todo Project/src/components/Todos.jsx
+++ Todo Project/src/components/Todos.jsx
@@ -1,14 +1,17 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { removeTodo } from "../features/todo/todoSlice";
 
 function Todos() {
-  const todos = useSelector((state) => state.todos )
+  const todos = useSelector((state) => state.todos)
+  const dispatch = useDispatch()
+
   return (
     <>
       <div>Todos</div>
       <ul className="list-none">
         {todos.map((todo) =>  <li key={todo.id} className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded">
           <div className="text-white">{todo.text}</div>
-          <button className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md">
+          <button onClick={() => dispatch(removeTodo(todo.id))} className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
